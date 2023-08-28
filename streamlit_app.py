1e2ad313a53e95c803a8c42f5269a6b33dd9f84a
Davenb42 <david.benavides.n@gmail.com>
2023-08-28 16:44:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ def get_fruit_load_list():
 #Add a button
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_fruid_load_list():
+  my_data_rows = get_fruid_load_list()
   streamlit.dataframe(my_data_rows)
 
 def insert _row_snowflake(new_fruit):
